4ff3f198861bf74c1b5d68855a2005984371dcc9
Vincent Scheib <scheib@chromium.org>
2014-12-02 13:14:08 -0800

    cl format

--- device/bluetooth/bluetooth_export.h
+++ device/bluetooth/bluetooth_export.h
@@ -6,22 +6,28 @@
 #define DEVICE_BLUETOOTH_DEVICE_BLUETOOTH_EXPORT_H_
 
 #if defined(COMPONENT_BUILD)
-#  if defined(WIN32)
-#    if defined(DEVICE_BLUETOOTH_IMPLEMENTATION)
-#      define DEVICE_BLUETOOTH_EXPORT __declspec(dllexport)
-#    else
-#      define DEVICE_BLUETOOTH_EXPORT __declspec(dllimport)
-#    endif  // defined(DEVICE_BLUETOOTH_IMPLEMENTATION)
-#  else // defined(WIN32)
-#    if defined(DEVICE_BLUETOOTH_IMPLEMENTATION)
-#      define DEVICE_BLUETOOTH_EXPORT __attribute__((visibility("default")))
-#    else
-#      define DEVICE_BLUETOOTH_EXPORT
-#    endif
-#  endif
+#if defined(WIN32)
 
-#else // defined(COMPONENT_BUILD)
-#  define DEVICE_BLUETOOTH_EXPORT
+#if defined(DEVICE_BLUETOOTH_IMPLEMENTATION)
+#define DEVICE_BLUETOOTH_EXPORT __declspec(dllexport)
+#else
+#define DEVICE_BLUETOOTH_EXPORT __declspec(dllimport)
+#endif  // defined(DEVICE_BLUETOOTH_IMPLEMENTATION)
+
+#else  // defined(WIN32)
+
+#if defined(DEVICE_BLUETOOTH_IMPLEMENTATION)
+#define DEVICE_BLUETOOTH_EXPORT __attribute__((visibility("default")))
+#else
+#define DEVICE_BLUETOOTH_EXPORT
 #endif
+
+#endif  // defined(WIN32)
+
+#else  // defined(COMPONENT_BUILD)
+
+#define DEVICE_BLUETOOTH_EXPORT
+
+#endif  // defined(COMPONENT_BUILD)
 
 #endif  // DEVICE_BLUETOOTH_DEVICE_BLUETOOTH_EXPORT_H_
